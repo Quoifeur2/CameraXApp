054c59919a956cf8599b254f12707ced3f8199ea
clement <clement.bourvic@telecom-sudparis.eu>
2023-04-11 07:44:00 +0000

    Detection de trapèze

--- OpenCV/build/intermediates/cxx/Debug/6m6dc465/logs/x86/configure_command.bat
+++ OpenCV/build/intermediates/cxx/Debug/6m6dc465/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\cleme\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\cleme\\AndroidStudioProjects\\CameraXApp\\OpenCV\\libcxx_helper" ^
+  "-HC:\\Users\\cleme\\AndroidStudioProjects\\CameraXAppV2\\OpenCV\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=29" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\cleme\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\cleme\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\cleme\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\cleme\\AndroidStudioProjects\\CameraXApp\\OpenCV\\build\\intermediates\\cxx\\Debug\\6m6dc465\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\cleme\\AndroidStudioProjects\\CameraXApp\\OpenCV\\build\\intermediates\\cxx\\Debug\\6m6dc465\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\cleme\\AndroidStudioProjects\\CameraXAppV2\\OpenCV\\build\\intermediates\\cxx\\Debug\\6m6dc465\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\cleme\\AndroidStudioProjects\\CameraXAppV2\\OpenCV\\build\\intermediates\\cxx\\Debug\\6m6dc465\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\cleme\\AndroidStudioProjects\\CameraXApp\\OpenCV\\.cxx\\Debug\\6m6dc465\\x86" ^
+  "-BC:\\Users\\cleme\\AndroidStudioProjects\\CameraXAppV2\\OpenCV\\.cxx\\Debug\\6m6dc465\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
